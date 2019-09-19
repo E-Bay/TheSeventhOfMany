c2040598bc8eace5e3cbe4471dfb9f91160096f2
Eric A. Bayer <eric.bayer@centre.edu>
2019-09-18 21:31:37 -0400

    Final.Proj

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 var addCat = function()
-{
- 
-
-
-
+{ var node = document.createElement("h2");
+ node.innerText = "Aliens are real, Area 51 has all my Secrets";
+ var body = document.querySelector("body");
+ body.appendChild(node); 
 }
-var addDog = function()
-{
- 
-
-
-
-}
-var addMan = function()
-{
- 
-
-
-
-}
+var addDog = function() 
+{  
+var node = document.createElement("h2");
+node.innerText = "My Top Secret info is not really Top Secret";
+var body = document.querySelector("body");
+body.appendChild(node);
+} 
+function mouseDown() 
+{ document.getElementById("Click").style.color = "Green";} 
+function mouseUp() 
+{ document.getElementById("Click").style.color = "Blue"; }
